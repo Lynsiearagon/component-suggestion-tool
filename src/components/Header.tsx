734418b7289fccbff755afe95a15d6113b43c7e0
Lynsiearagon <lynsie.aragon@gmail.com>
2025-07-15 23:58:56 -0400

    Add Code link to nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,15 @@ const Header = () => {
           Lynsie Aragon
         </span>
       </div>
-      <div className="flex gap-8 font-semibold text-gray-600 font-mono uppercase border-l-4 border-[#1434cb] pl-2  text-base sm:text-lg md:text-xl">
+      <div className="flex gap-8 font-semibold text-gray-600 font-mono uppercase  text-base sm:text-lg md:text-xl">
+        <a
+          href="https://github.com/Lynsiearagon/component-suggestion-tool/blob/main/README.md"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-pink-500"
+        >
+          Code
+        </a>
         <a
           href="https://www.linkedin.com/in/lynsie-aragon-dev/"
           target="_blank"
